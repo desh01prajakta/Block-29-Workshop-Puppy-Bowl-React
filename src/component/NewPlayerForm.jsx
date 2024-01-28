2997a77b801345d670c43f1744f1ff0625d79924
desh01prajakta <prajudeshmukh11@gmail.com>
2024-01-27 22:05:55 -0600

    adds value

---
+++ src/component/NewPlayerForm.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { useAddEventMutation } from "../redux"
+
+function AddPlayerForm() {
+    const [name, setName] = useState("");
+    const [breed, setBreed] = useState("");
+    const [status, setStatus] = useState("");
+    const [teamId, setTeamId] = useState("");
+    const [cohortId, setCohortId] = useState("")
+    const [ addPlayer ] = useAddEventMutation()
+  
+    function handleSubmit(e) {
+      e.preventDefault();
+      addPlayer({
+        name,
+        breed,
+        status,
+        teamId,
+        cohortId,
+        })
+    }
+      return (
+      <>
+      <div className="column">
+        <h2>Add Player form</h2>
+        <form className="column">
+          <label>Name:</label>
+          <input onChange={(e) => setName(e.target.value)} name="name"></input>
+          <label>Breed:</label>
+          <input onChange={(e) => setBreed(e.target.value)} name="breed"></input>
+          <label>Status:</label>
+          <input onChange={(e) => setStatus(e.target.value)} name="status"></input>
+          <label>TeamId:</label>
+          <input onChange={(e) => setTeamId(e.target.value)} name="teamId" type=""></input>
+          <label>CohortId:</label>
+          <input onChange={(e) => setCohortId(e.target.value)} name="cohortId" type=""></input>
+          <button onClick={handleSubmit} type="submit">Add Player</button>
+        </form>
+      </div>
+      </>
+    )
+  }
+  
+  export default AddPlayerForm
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import store from './app/store.js'
-import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from "react-redux"
+// import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Provider store={store}>
     <app />
-    </BrowserRouter>
+    </Provider>
   </React.StrictMode>)
